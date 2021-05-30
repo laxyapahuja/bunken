4c955666f5c3acbced0037f01e84c2f8302e92ad
Govert Versluis <govert+git@ver.slu.is>
2021-05-30 13:54:01 +0200

    Rework UI setup to prevent XSS

--- support/goodreads/script.js
+++ support/goodreads/script.js
@@ -1,60 +1,84 @@
 const API = 'https://api.bunken.tk/'
 
 let ebookElement = document.createElement('div')
+let ebookResultsElement;
 let relatedElement = document.querySelector('[id^="relatedWorks-"]')
 let bookTitle = document.querySelector("[property='og:title']").getAttribute("content");
 let ISBNCode = document.querySelector("[property='books:isbn']").getAttribute("content");
-let initial = false
 
 function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
 function ebookElementInflator(results) {
-    let string = `<div class="h2Container gradientHeaderContainer">
-    <h2 class="brownBackground">E-Books</h2>
-</div>
-<select id="source">
-<option value="libgen">Source: LibGen</option>
-<option value="motw">Source: Memory Of The World</option>
-<option value="libgen/fiction">Source: LibGen Fiction</option>
-<option value="openlibrary">Source: OpenLibrary</option>
-</select> <button onclick="sourceSelect()">Select</button>
-<div class="bigBoxContent containerWithHeaderContent" style="overflow-y: auto; max-height: 300px;" id="resultsDiv">`
+    ebookResultsElement.innerHTML = ''
     results.forEach(book => {
-        string += `<div class="elementList">
-        <div>
-            <a class="actionLinkLite bookPageGenreLink" target="_blank" href="${book.link}">${book.title}</a>
-        </div>`
+        let resultElement = document.createElement('div')
+        resultElement.className = 'elementList'
+        let bookElement = document.createElement('div')
+        let bookLinkElement = document.createElement('a')
+        bookLinkElement.href = book.link
+        bookLinkElement.textContent = book.title
+        bookLinkElement.className = 'actionLinkLite bookPageGenreLink'
+        bookLinkElement.target = "_blank"
+        bookElement.appendChild(bookLinkElement)
+        resultElement.appendChild(bookElement)
+
         if (book.downloads != null) {
             book.downloads.forEach(download => {
-                string += `<div><a style="color: blue;" href="${download.link}">${download.format}</a></div>`
+                let downloadElement = document.createElement('div')
+                let downloadLinkElement = document.createElement('a')
+                downloadLinkElement.href = download.link
+                downloadLinkElement.textContent = download.format
+                downloadLinkElement.style.color = 'blue'
+                downloadElement.appendChild(downloadLinkElement)
+                resultElement.appendChild(downloadElement)
             })
         }
-        string += `<div>${book.author}</div>
-        <div class="clear"></div>
-    </div>`
+
+        let authorElement = document.createElement('div')
+        authorElement.textContent = book.author
+        resultElement.appendChild(authorElement)
+
+        let clearElement = document.createElement('div')
+        clearElement.className = 'clear'
+        resultElement.appendChild(clearElement)
+
+        ebookResultsElement.appendChild(resultElement)
     })
-    string += '</div>'
-    ebookElement.innerHTML = string
 }
 
 function sourceSelect() {
     let e = document.getElementById("source");
     let value = e.options[e.selectedIndex].value;
-    mainInflator(value)
+    search(value)
 }
 
-function mainInflator(source) {
-    fetch(`${API}${source}?title=${bookTitle}&isbn=${ISBNCode}`).then(response => {
+function setupUI() {
+    let template = `<div class="h2Container gradientHeaderContainer">
+                        <h2 class="brownBackground">E-Books</h2>
+                    </div>
+                    <select id="source" onchange="sourceSelect()">
+                    <option value="libgen">Source: LibGen</option>
+                    <option value="motw">Source: Memory Of The World</option>
+                    <option value="libgen/fiction">Source: LibGen Fiction</option>
+                    <option value="openlibrary">Source: OpenLibrary</option>
+                    </select> 
+                    <div id="ebookResults" class="bigBoxContent containerWithHeaderContent" style="overflow-y: auto; max-height: 300px;" id="resultsDiv">Searching...</div>`
+    ebookElement.innerHTML = template
+    ebookElement.className = 'bigBox'
+    insertAfter(relatedElement, ebookElement)
+    ebookResultsElement = document.getElementById('ebookResults')
+}
+
+function search(source) {
+    ebookResultsElement.innerHTML = 'Searching...'
+    fetch(`${API}${source}?title=${encodeURIComponent(bookTitle)}&isbn=${encodeURIComponent(ISBNCode)}`).then(response => {
         response.json().then(res => {
             ebookElementInflator(res)
-            if (!initial) {
-                insertAfter(relatedElement, ebookElement)
-                initial = true
-            }
         })
     })
 }
 
-mainInflator('libgen')
+setupUI();
+search('libgen')
